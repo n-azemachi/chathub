23b6bc9a069471ec7caacb961b4c192b9bafbd53
EC2 Default User <ec2-user@ip-172-31-44-65.ap-northeast-1.compute.internal>
2019-05-20 14:14:32 +0900

    chat_app base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'rooms#index'
+  resources :rooms, only: %i[new show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
